2b4d734e09aec95ee249b0983466019ba1b5ecba
eddie <bda605@gmail.com>
2017-07-03 23:52:17 +0800

    加入LV1的第一支DI

---
+++ DI_EX1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    
+    interface IMessageServie 
+    {
+        void Send(User user, string msg);
+    }
+
+    class EmailService : IMessageServie 
+    {
+        public void Send(User user, string msg) 
+        {
+            Console.WriteLine("寄送郵件,內容為:" + msg);
+        }
+    }
+    class ShortMessageService : IMessageServie 
+    {
+        public void Send(User user, string msg)
+        {
+            Console.WriteLine("寄送簡訊,內容為:" + msg);
+        }
+    }
+    class AuthenticationService 
+    {
+        private IMessageServie msgService;
+        public AuthenticationService(IMessageServie msgService) 
+        {
+            this.msgService = msgService;
+        }
+        public bool ToFactorLogin(string UserId, string Pwd) 
+        {
+            User user = CheckPassword(UserId, Pwd);
+            if (user != null) 
+            {
+                this.msgService.Send(user,"你的驗證碼123456");
+                return true;
+            }
+            return false;
+        }
+        public  bool VerifyToken(string token)
+        {
+            return true;
+        }
+        User CheckPassword(string UserId, string Pwd) 
+        {
+            var user = new User()
+            {
+                UserId = UserId,
+                Email = "bda605@gmail.com"
+            };
+            return user;
+        }
+    }
+    class User 
+    {
+        public string UserId;
+        public string Email;
+        public string CellPhone;
+    }
+    class MainApp 
+    {
+        public void Login(string userId, string pwd, string messageServiceType) 
+        {
+            IMessageServie msgService = null;
+            switch (messageServiceType) 
+            {
+                case "EmailService":
+                    msgService = new EmailService();
+                    break;
+                case "ShortMessageService":
+                    msgService = new ShortMessageService();
+                    break;
+            }
+            AuthenticationService authService = new AuthenticationService(msgService);
+            if (authService.ToFactorLogin(userId, pwd)) 
+            {
+                string userInputToken = "123456";
+                if (authService.VerifyToken(userInputToken)) 
+                {
+                   //ok
+                }
+            }
+            //Error
+        }
+    }
+}
